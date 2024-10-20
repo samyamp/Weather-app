d4824e4127e938a0c538a9551683e75ef60452a2
Samyam Pandey <pandeysamyam@hotmail.com>
2024-10-20 15:09:11 +0100

    get humidity and get wind speed functions

--- script.js
+++ script.js
@@ -17,4 +17,39 @@ function get_temperature(location) {
   });
 }
 
-get_temperature("Bradford")
+function get_humidity(location) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}&units=metric`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("get_humidity response not working");
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data.main.humidity);
+  })
+  .catch(error => {
+    console.error('Error caught:', error);
+  });
+}
+
+function get_wind(location) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}&units=imperial`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("get_wind response not working");
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data.wind.speed);
+  })
+  .catch(error => {
+    console.error('Error caught:', error);
+  });
+}
+
+
+get_wind("Delhi")
+get_humidity("Delhi")
+get_temperature("Delhi")
