deaa3dea919d847be8a9116cfb0daf9bd68e4e5f
Samyam Pandey <pandeysamyam@hotmail.com>
2024-10-22 00:10:40 +0100

    Icons update live

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const weatherKey = "05d3648c98ea595a77e6ec3702143b76";
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=05d3648c98ea595a77e6ec3702143b76
+const searchButton = document.querySelector(".searchbar button");
+const search = document.querySelector(".searchbar input");
 
 async function checkWeather(location){
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}&units=metric`)
@@ -11,6 +13,23 @@ async function checkWeather(location){
     document.querySelector(".temperature").innerHTML= Math.round(data.main.temp) + "°C";
     document.querySelector(".windSpeed").innerHTML = data.wind.speed + " mph";
     document.querySelector(".humidtyPercentage").innerHTML = data.main.humidity + "%";
+    console.log(data.weather[0].main)
+    if (data.weather[0].main == "Rain"){
+      document.querySelector(".icon").src="icons/rainy-5.svg"
+    }else if(data.weather[0].main == "Clouds"){
+      document.querySelector(".icon").src="icons/cloudy.svg"
+    }else if(data.weather[0].main == "Clear"){
+      document.querySelector(".icon").src="icons/day.svg"
+    }else if(data.weather[0].main == "Snow"){
+      document.querySelector(".icon").src="icons/snowy-2.svg"
+    }else if(data.weather[0].main == "Thunderstorm"){
+      document.querySelector(".icon").src="icons/thunder.svg"
+    }else{
+      console.log("Noithing")
+      console.log(data.weather[0].main)
+    }
+
+
   })
   .catch(error => {
     console.error('Error caught:', error);
@@ -19,15 +38,12 @@ async function checkWeather(location){
 
 checkWeather("London")
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  const searchButton = document.querySelector(".searchbar button");
-  const search = document.querySelector(".searchbar input");
-  if (searchButton) {
-    searchButton.addEventListener("click", ()=>{
-      checkWeather(search.value);
-    })
-  }
-});
+
+searchButton.addEventListener("click", ()=>{
+  checkWeather(search.value);
+})
+
+
 
 
 
