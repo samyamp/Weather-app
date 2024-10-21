d498a73b228959a817bb8f61d55e7d9a08546967
Samyam Pandey <pandeysamyam@hotmail.com>
2024-10-21 14:54:37 +0100

    Weather can now be checked and updates live on the website

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const weatherKey = "05d3648c98ea595a77e6ec3702143b76";
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=05d3648c98ea595a77e6ec3702143b76
 
+async function checkWeather(location){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}&units=metric`)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    document.querySelector(".name").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML= data.main.temp;
+    document.querySelector(".windSpeed").innerHTML = data.wind.speed;
+    document.querySelector(".humidtyPercentage").innerHTML = data.main.humidity;
+  })
+  .catch(error => {
+    console.error('Error caught:', error);
+  });
+}
+
+checkWeather("Sofia")
+
+
+
+
+
+/*
 function get_temperature(location) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}&units=metric`)
   .then(response => {
@@ -48,10 +71,4 @@ function get_wind(location) {
     console.error('Error caught:', error);
   });
 }
-
-
-
-
-get_wind("London")
-get_humidity("Delhi")
-get_temperature("Delhi")
+*/
